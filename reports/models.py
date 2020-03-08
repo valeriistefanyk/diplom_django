d1a7101edb9aedef023724f630da33e59d52304b
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-03-08 22:55:16 +0200

    Added partial template (to machines.html)

--- reports/models.py
+++ reports/models.py
@@ -4,6 +4,7 @@ from machines.models import Machine
 
 # Create your models here.
 class Report(models.Model):
+    
     filled_up = models.ForeignKey(SeniorDriver, on_delete=models.CASCADE)
     date_of_completion = models.DateTimeField(auto_now=True)
     date = models.DateField()
@@ -11,6 +12,8 @@ class Report(models.Model):
     motohour = models.FloatField()
     fuel = models.FloatField()
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+
+    breakage = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.filled_up} - {self.date}"
