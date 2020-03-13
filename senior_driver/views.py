bbd8f692daa9346d0ee32e4db07497d97b1d0de5
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-03-13 14:30:22 +0200

    Changed make_report page

--- senior_driver/views.py
+++ senior_driver/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 
 from machines.models import Machine
-
+from engineer.models import Report
 
 def hello_mess(request):
 
@@ -55,6 +55,23 @@ def make_report(request):
     if request.method == "POST":
         # TODO: make post data
         print("METHOD POST")
+        if not request.user.is_superuser:
+            filled_up = request.user.seniordriver
+            date = request.POST.get('date')
+            motohour = request.POST.get('motohour')
+            fuel = request.POST.get('fuel')
+
+            machine_id = request.POST.get('machine')
+            machine = Machine.objects.get(pk=machine_id)
+            
+            breakage = True if request.POST.get('breakage') == 'on' else False
+
+            print(
+                f"filled up: {filled_up}\ndate: {date}\nmotohour: {motohour}\nfuel: {fuel}\nmachine id: {machine_id}\nbreakage: {breakage}"
+            )
+
+            report = Report.objects.create(filled_up=filled_up, date=date, motohour=motohour, fuel=fuel, machine=machine, breakage=breakage)
+
         return redirect('driver:home-page')
 
     return render(request, 'senior-driver/make_report.html', context)
