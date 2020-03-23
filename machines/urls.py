f19c3be62e0e2ebe485e91f8869449c0ab4c6199
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-03-23 14:38:01 +0200

    Added detail machines into view and template

--- machines/urls.py
+++ machines/urls.py
@@ -6,6 +6,7 @@ app_name = 'machines'
 
 urlpatterns = [
     path('', views.all_machines, name='all-machines'),
+    path('detail/', views.show_detail_machines, name='detail_machines'),
     path('<int:id>/', views.detail_machine, name='detail'),
     path('description/', views.show_machines_description, name='show_machine_description'),
 ]
