67ee066c46e947f5cd78b9bc7921cdd725184f2d
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-04-21 23:43:17 +0300

    Added photo to detail_machine, fixed bugs

--- machines/urls.py
+++ machines/urls.py
@@ -5,8 +5,8 @@ from machines import views
 app_name = 'machines'
 
 urlpatterns = [
-    path('', views.all_machines, name='all-machines'),
-    path('detail/', views.show_detail_machines, name='detail_machines'),
+    # path('', views.all_machines, name='all-machines'),
+    # path('detail/', views.show_detail_machines, name='detail_machines'),
     path('<int:id>/', views.detail_machine, name='detail'),
     path('description/', views.show_machines_description, name='show_machine_description'),
 ]
