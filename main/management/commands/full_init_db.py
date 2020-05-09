84a0770617dbaf5a0f83b36046105d4f7166762c
Valerii Stefanyk <valeriistefanyk@gmail.com>
2020-05-09 13:55:42 +0300

    deleted unnecessary files

--- main/management/commands/full_init_db.py
+++ main/management/commands/full_init_db.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from main.management.commands.init_users_perm_db import Command as UserCommand
 from main.management.commands.init_machines_db import Command as MachineCommand
 from main.management.commands.init_reports_db import Command as ReportCommand
+from main.management.commands.create_csv_machinist import Command as MachinistCommand
 
 
 class Command(BaseCommand):
@@ -11,4 +12,6 @@ class Command(BaseCommand):
         UserCommand().handle()
         MachineCommand().handle()
         ReportCommand().handle()
+        MachinistCommand().handle()
+
         
